092e6797f7b3c90c951274d57b18a3aa1c554e60
RawanDah <rawanabdullah4977@gmail.com>
2023-12-13 20:18:55 +0300

    add

--- app/src/main/java/com/example/luqya/AddEvent.java
+++ app/src/main/java/com/example/luqya/AddEvent.java
@@ -26,26 +26,5 @@ public class AddEvent extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.add_event_activity);
 
-        Objects.requireNonNull(getSupportActionBar()).setBackgroundDrawable(new ColorDrawable(R.color.black));
-        imageView = findViewById(R.id.imageView);
-        button = findViewById(R.id.floatingActionButton2);
-        button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                ImagePicker.with(AddEvent.this)
-                        .crop()                    //Crop image(Optional), Check Customization for more option
-                        .compress(1024)            //Final image size will be less than 1 MB(Optional)
-                        .maxResultSize(1080, 1080)    //Final image resolution will be less than 1080 x 1080(Optional)
-                        .start();
-            }
-        });
-
-    }
-
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-        Uri uri = data.getData();
-        imageView.setImageURI(uri);
     }
 }
