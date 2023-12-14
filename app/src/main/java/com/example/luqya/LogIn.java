7c24026fd5138b9ed14ba14d807ddfeeb5602154
IbtisamAbdulaziz <bassoma1999i@gmail.com>
2023-12-14 12:45:56 +0300

    LogIn.java is updated

--- app/src/main/java/com/example/luqya/LogIn.java
+++ app/src/main/java/com/example/luqya/LogIn.java
@@ -2,13 +2,27 @@ package com.example.luqya;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class LogIn extends AppCompatActivity {
+
+    TextView textview_signup;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_log_in);
+        textview_signup = findViewById(R.id.textview_signup);
+
+        textview_signup.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(LogIn.this,SignUp.class);
+                startActivity(i);
+            }
+        });
     }
 }
