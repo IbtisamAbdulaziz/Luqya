7051d88f281970f47f9e4296dbe7df8be42f2371
IbtisamAbdulaziz <bassoma1999i@gmail.com>
2023-12-14 10:10:59 +0300

    InitiativeFounderProfile is added

---
+++ app/src/main/java/com/example/luqya/InitiativeFounderProfile.java
@@ -0,0 +1,14 @@
+package com.example.luqya;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class InitiativeFounderProfile extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_initiative_founder_profile);
+    }
+}
