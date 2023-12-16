442f3964115ed83ec735c91b88a16b5fd9ee1d96
IbtisamAbdulaziz <bassoma1999i@gmail.com>
2023-12-16 13:47:02 +0300

    SignUp.java is updated.

--- app/src/main/java/com/example/luqya/SignUp.java
+++ app/src/main/java/com/example/luqya/SignUp.java
@@ -6,6 +6,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.text.TextUtils;
+import android.util.Log;
 import android.util.Patterns;
 import android.view.View;
 import android.widget.Button;
@@ -18,6 +19,9 @@ import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseAuthInvalidCredentialsException;
+import com.google.firebase.auth.FirebaseAuthUserCollisionException;
+import com.google.firebase.auth.FirebaseAuthWeakPasswordException;
 import com.google.firebase.auth.FirebaseUser;
 
 public class SignUp extends AppCompatActivity {
@@ -27,6 +31,7 @@ public class SignUp extends AppCompatActivity {
 
     private RadioGroup userTypeRadioGroup;
     private RadioButton userType;
+    private static final String TAG= "SignUp";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -147,6 +152,23 @@ public class SignUp extends AppCompatActivity {
 
                     startActivity(intent);
                     finish();
+                } else {
+                    try {
+                        throw task.getException();
+                    } catch (FirebaseAuthWeakPasswordException e){
+                        editTextPassword.setError("Your password is too weak. Kindly use a mix of alphabets, numbers " +
+                                "and special characters");
+                        editTextPassword.requestFocus();
+                    } catch (FirebaseAuthInvalidCredentialsException e){
+                        editTextEmail.setError("Your email is invalid or already in use. Kindly re-enter.");
+                        editTextEmail.requestFocus();
+                    } catch (FirebaseAuthUserCollisionException e){
+                        editTextEmail.setError("User is already registered with this email. use another email.");
+                        editTextEmail.requestFocus();
+                    } catch (Exception e){
+                        Log.e(TAG,e.getMessage());
+                        Toast.makeText(SignUp.this, e.getMessage(), Toast.LENGTH_LONG).show();
+                    }
                 }
             }
         });
