b4fa9b35366d011cad8787943f8dcf31883f1642
IbtisamAbdulaziz <bassoma1999i@gmail.com>
2023-12-18 16:14:42 +0300

    LogIn.java + activity_log_in.xml. (Video 8)

--- app/src/main/java/com/example/luqya/LogIn.java
+++ app/src/main/java/com/example/luqya/LogIn.java
@@ -1,27 +1,96 @@
 package com.example.luqya;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.util.Patterns;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class LogIn extends AppCompatActivity {
 
+    private EditText editTextLoginEmail, editTextLoginPwd;
+    private ProgressBar progressBar;
+    private FirebaseAuth authProfile;
     TextView textview_signup;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_log_in);
+
+        getSupportActionBar().setTitle("Login");
         textview_signup = findViewById(R.id.textview_signup);
+        editTextLoginEmail = findViewById(R.id.edittext_email_login);
+        editTextLoginPwd = findViewById(R.id.edittext_password_login);
+        progressBar = findViewById(R.id.progressBarLogin);
+
+        authProfile = FirebaseAuth.getInstance();
+
+        Button btnLogin = findViewById(R.id.button_login);
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String textEmail = editTextLoginEmail.getText().toString();
+                String textPwd = editTextLoginPwd.getText().toString();
+
+                if(TextUtils.isEmpty(textEmail)){
+                    Toast.makeText(LogIn.this, "Please enter your email", Toast.LENGTH_LONG).show();
+                    editTextLoginEmail.setError("Email is required");
+                    editTextLoginEmail.requestFocus();
+
+                } else if (!Patterns.EMAIL_ADDRESS.matcher(textEmail).matches()){
+                    Toast.makeText(LogIn.this, "The email is incorrect", Toast.LENGTH_LONG).show();
+                    editTextLoginEmail.setError("Valid email is required");
+                    editTextLoginEmail.requestFocus();
+
+                } else if(TextUtils.isEmpty(textPwd)){
+                    Toast.makeText(LogIn.this, "Please enter your password", Toast.LENGTH_LONG).show();
+                    editTextLoginPwd.setError("Password is required");
+                    editTextLoginPwd.requestFocus();
+
+                } else {
+                    progressBar.setVisibility(View.VISIBLE);
+                    loginUser(textEmail , textPwd);
+                }
+
+            }
+        });
+
 
         textview_signup.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Intent i = new Intent(LogIn.this,SignUp.class);
                 startActivity(i);
+            }
+        });
+    }
+
+    private void loginUser(String email, String pwd) {
+        authProfile.signInWithEmailAndPassword(email, pwd).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if(task.isSuccessful()){
+                    Toast.makeText(LogIn.this, "You are logged in now", Toast.LENGTH_SHORT).show();
+                } else {
+                    Toast.makeText(LogIn.this, "something went wrong!", Toast.LENGTH_SHORT).show();
+                }
+
+                progressBar.setVisibility(View.GONE);
+
             }
         });
     }
