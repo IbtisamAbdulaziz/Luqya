fc30020fd495c37679f606cc9667815a91797713
IbtisamAbdulaziz <bassoma1999i@gmail.com>
2024-01-20 15:42:45 +0300

    FounderMainActivity is done.

--- app/src/main/java/com/example/luqya/FounderMainActivity.java
+++ app/src/main/java/com/example/luqya/FounderMainActivity.java
@@ -1,28 +1,47 @@
 package com.example.luqya;
 
-import android.annotation.SuppressLint;
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+import com.squareup.picasso.Picasso;
+
 public class FounderMainActivity extends AppCompatActivity {
-    ImageView Initiative_Logo;
-    TextView Initiative_Name;
-    Button Initiative_Profile, Add_Event, My_Events, Edit_Profile, Statistics;
+    private ImageView Initiative_Logo;
+    private TextView textViewInitiativeName, textViewInitiativeFounder;
+    private Button Initiative_Profile, Add_Event, My_Events, Edit_Profile, Statistics;
+    private String initiativeName, initiativeFounder;
+    private FirebaseAuth authProfile;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.founder_main);
+        getSupportActionBar().setTitle("Initiative Dashboard");
 
         Initiative_Logo = findViewById(R.id.imageView);
-        Initiative_Name = findViewById(R.id.textView);
+
+        textViewInitiativeName = findViewById(R.id.textView);
+        textViewInitiativeFounder = findViewById(R.id.textView_founder_name_welcome);
+
         Initiative_Profile = findViewById(R.id.button);
         Add_Event = findViewById(R.id.button2);
         My_Events = findViewById(R.id.button3);
@@ -60,6 +79,92 @@ public class FounderMainActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+
+        Initiative_Logo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(FounderMainActivity.this, UploadInitiativePicture.class);
+                startActivity(i);
+            }
+        });
+
+        authProfile = FirebaseAuth.getInstance();
+        FirebaseUser firebaseUser = authProfile.getCurrentUser();
+
+        if(firebaseUser == null){
+            Toast.makeText(FounderMainActivity.this, "Something went wrong! User's details are not available at the moment", Toast.LENGTH_SHORT).show();
+        } else {
+            showUserProfile(firebaseUser);
+        }
+
+    }
+
+    private void showUserProfile(FirebaseUser firebaseUser) {
+
+        String userID = firebaseUser.getUid();
+        DatabaseReference referenceProfile = FirebaseDatabase.getInstance().getReference("Registered initiatives");
+        referenceProfile.child(userID).addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                ReadWriteUserDetails readUserDetails = snapshot.getValue(ReadWriteUserDetails.class);
+
+                if(readUserDetails != null){
+                    initiativeName = readUserDetails.initiativeName;
+                    initiativeFounder = readUserDetails.initiativeFounderName;
+
+                    textViewInitiativeName.setText(initiativeName);
+                    textViewInitiativeFounder.setText("Welcome back " + initiativeFounder + " !");
+
+
+
+                    Uri uri = firebaseUser.getPhotoUrl();
+                    Picasso.with(FounderMainActivity.this).load(uri).into(Initiative_Logo);
+
+                } else {
+                    Toast.makeText(FounderMainActivity.this, "Something went wrong!", Toast.LENGTH_SHORT).show();
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+                Toast.makeText(FounderMainActivity.this, "Something went wrong!", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_profile_founder, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        int id = item.getItemId();
+
+        if (id == R.id.menu_item_reset_password) {
+
+            Intent intent = new Intent(FounderMainActivity.this, Forgot_Yor_Password.class);
+            startActivity(intent);
+
+        } else if (id == R.id.menu_item_logout) {
+
+            authProfile.signOut();
+            Toast.makeText(FounderMainActivity.this, "Logged Out", Toast.LENGTH_SHORT).show();
+            Intent intent = new Intent(FounderMainActivity.this, MainInterface.class);
+            startActivity(intent);
+
+            intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_NEW_TASK);
+            startActivity(intent);
+            finish();
+
+        } else {
+            Toast.makeText(FounderMainActivity.this, "Something went wrong", Toast.LENGTH_SHORT).show();
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 }
 
