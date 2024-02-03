8e21bf9555a231925194f4c235c9aca0265bbc72
DhayHamad <dhayaldaeilij@gmail.com>
2024-02-03 05:59:12 +0300

    Registration now button in Details_event_class is successful

--- app/src/main/java/com/example/luqya/Details_event_class.java
+++ app/src/main/java/com/example/luqya/Details_event_class.java
@@ -1,14 +1,25 @@
 package com.example.luqya;
 
 import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.bumptech.glide.Glide;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class Details_event_class extends AppCompatActivity {
 
@@ -25,11 +36,6 @@ public class Details_event_class extends AppCompatActivity {
         setContentView(R.layout.activity_details_event_class);
         getSupportActionBar().setTitle("Event Details");
 
-        /*
-        List<DataClass> dataList;
-        RecyclerView recyclerView;
-                                   */
-
 
         titleTextView = findViewById(R.id.event_title);
         categoryTextView = findViewById(R.id.event_category);
@@ -45,7 +51,6 @@ public class Details_event_class extends AppCompatActivity {
         imageView = findViewById(R.id.event_image);
         register_button = findViewById(R.id.Register);
 
-       // recyclerView = findViewById(R.id.myeventView2);
 
         Bundle bundle = getIntent().getExtras();
         if (bundle != null) {
@@ -76,6 +81,49 @@ public class Details_event_class extends AppCompatActivity {
 
             initiativeTextView.setText(initiative + " ");
         }
+        register_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                FirebaseAuth auth = FirebaseAuth.getInstance();
+                FirebaseUser currentUser = auth.getCurrentUser();
+                if (currentUser == null) {
+                    // If user is not logged in, navigate to the login activity
+                    Intent intent = new Intent(Details_event_class.this, LogIn.class);
+                    startActivity(intent);
+                } else {
+                    // If user is logged in, proceed with event registration
+                    register_button(currentUser);
+                }
+            }
+        });
+    }
+
+    private void register_button(FirebaseUser currentUser) {
+        String eventName = title; // Get the event name from the activity
+
+
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+        DatabaseReference dbRef = database.getReference("EventData").child(eventName).child("attendees");
+
+        // Add the current user to the attendees of the current event
+        dbRef.child(currentUser.getUid()).setValue(true)
+                .addOnSuccessListener(new OnSuccessListener<Void>() {
+                    @Override
+                    public void onSuccess(Void unused) {
+                        Toast.makeText(Details_event_class.this, "You've successfully registered to the event!", Toast.LENGTH_LONG).show();
+                        Log.d("Registration", "User successfully registered!");
+                        // Navigate to the MyEventsActivity or any other desired activity
+                        Intent intent = new Intent(Details_event_class.this, MyEvent_bar.class);
+                        startActivity(intent);
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Log.w("Registration", "Error registering user", e);
+
+                    }
+                });
     }
 }
 
